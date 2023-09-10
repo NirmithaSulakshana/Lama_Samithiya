9b5395cdfc24baf2eaced192a1053e3b3a572de2
NirmithaSulakshana <nairmitha2000@gmail.com>
2023-09-10 22:20:56 +0530

    Web Files
    Desciption of web site

---
+++ home page/myJs.js
@@ -0,0 +1,5 @@
+const nirmithaSlide = document.querySelector('.nirmitha-slide');
+const nirmithaImage = document.querySelector('.nirmitha-slide img');
+
+//buttons
+const prevBtn = document.querySelector('')
